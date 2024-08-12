b8194aa4b61a86a27a6a01e707b876916a86ac9e
tomyvalde <166783483+tomyvalde@users.noreply.github.com>
2024-08-12 14:40:33 -0300

    Add files via upload

---
+++ Tarea1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int opcion;
+
+    printf("Introduce un número (1 para Inglés, 2 para Español, 3 para Alemán): ");
+    scanf("%d", &opcion);
+
+    switch(opcion) {
+        case 1:
+            printf("Hello World\n");
+            break;
+        case 2:
+            printf("Hola Mundo\n");
+            break;
+        case 3:
+            printf("Hallo Welt\n");
+            break;
+        default:
+            printf("Opción no válida. Por favor, ingresa 1, 2 o 3.\n");
+            break;
+    }
+
+    return 0;
+}
